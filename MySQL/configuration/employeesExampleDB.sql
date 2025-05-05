afafe9573607d22d51b1ce422f57dc8b33ff7850
Jorge <UO282204@uniovi.es>
2025-05-05 14:41:55 +0200

    Switched to absolute file paths.

--- MySQL/configuration/employeesExampleDB.sql
+++ MySQL/configuration/employeesExampleDB.sql
@@ -124,4 +124,17 @@ source load_salaries1.dump ;
 source load_salaries2.dump ;
 source load_salaries3.dump ;
 
-source show_elapsed.sql ;
+source show_elapsed.sql ;SELECT 'LOADING departments' as 'INFO';
+source /docker-entrypoint-initdb.d/load_departments.dump ;
+SELECT 'LOADING employees' as 'INFO';
+source /docker-entrypoint-initdb.d/load_employees.dump ;
+SELECT 'LOADING dept_emp' as 'INFO';
+source /docker-entrypoint-initdb.d/load_dept_emp.dump ;
+SELECT 'LOADING dept_manager' as 'INFO';
+source /docker-entrypoint-initdb.d/load_dept_manager.dump ;
+SELECT 'LOADING titles' as 'INFO';
+source /docker-entrypoint-initdb.d/load_titles.dump ;
+SELECT 'LOADING salaries' as 'INFO';
+source /docker-entrypoint-initdb.d/load_salaries1.dump ;
+source /docker-entrypoint-initdb.d/load_salaries2.dump ;
+source /docker-entrypoint-initdb.d/load_salaries3.dump ;
