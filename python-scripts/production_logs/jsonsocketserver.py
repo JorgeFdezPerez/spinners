99d19ca58a7c7b942548b1a833ce9013b59580b2
Jorge <UO282204@uniovi.es>
2025-06-10 10:12:11 +0200

    Created production log generation.

---
+++ python-scripts/production_logs/jsonsocketserver.py
@@ -0,0 +1,111 @@
+import json
+import asyncio
+from collections.abc import Callable
+
+
+class JsonSocketServer:
+    """Async socket server for TCP communication of dictionaries.
+
+    Dictionaries are serialized as json and encoded.
+
+    Received data is sent to an event handler. On client connection,
+    the event {"socketServerEvent": "connected"} is sent.
+
+    Attributes:
+        sendQueue (asyncio.Queue): Put messages here to send them.
+    """
+    
+    async def start(self, eventHandler: any):
+        """Starts the server.
+
+        Args:
+            eventHandler (any): Object that will handle events received. Must have a method:
+                eventHandler.handleEvent(event: dict[str,str])
+        """
+        self._eventHandler = eventHandler
+        if(self._eventHandler == None):
+            raise TypeError("Event handler was not set")
+        # Host must be 0.0.0.0 on Docker
+        self._server = await asyncio.start_server(
+            client_connected_cb=self._onConnection,
+            host="0.0.0.0",
+            port=self._port)
+
+        loop = asyncio.get_running_loop()
+        loop.create_task(self._server.serve_forever())
+
+    def __init__(self, port: int, EOM="\r\n", encoding="utf-8"):
+        """Constructor for socket server.
+
+        Args:
+            port (int): Port for TCP communication.
+            EOM (str, optional): Marks the End Of Message.
+                Is deleted from the message when decoding. Defaults to "\\r\\n".
+            encoding (str, optional): Byte encoding. **Must be the same on client and server.** Defaults to "utf-8".
+        """
+        self._eventHandler = None
+        self._port = port
+        self._EOM = EOM
+        self._encoding = encoding
+        self._reader = None
+        self._writer = None
+
+        self.sendQueue = asyncio.Queue()
+
+    async def _onConnection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        """Launches loops to read and write messages to connected client.
+
+        Args:
+            reader (asyncio.StreamReader): Stream reader created by asyncio.start_server().
+            writer (asyncio.StreamWriter): Stream writer created by asyncio.start_server().
+        """
+        self._reader = reader
+        self._writer = writer
+        # Notify event handler
+        asyncio.create_task(self._eventHandler.handleEvent({"socketServerEvent": "connected"}))
+        # Launch read and write loops
+        asyncio.gather(self._readLoop(), self._writeLoop())
+
+    async def _readLoop(self):
+        """Reads messages from client, decodes them and sends them to the handler function.
+
+        Sends {"error": "socketServerDecoding"} to message handler if there
+        is an error when deserializing and decoding the dictionary.
+        """
+        while True:
+            # Check if connection ended
+            if (self._reader.at_eof()):
+                loop.create_task(self._fnHandleMessage(
+                    {"error": "socketClientDisconnected"}))
+                await self._writer.write_eof()
+            else:
+                # Wait for message that ends with EOM
+                received = await self._reader.readuntil(self._EOM.encode(self._encoding))
+                loop = asyncio.get_running_loop()
+                try:
+                    message = json.loads(received.decode(
+                        self._encoding).removesuffix(self._EOM))
+                except:
+                    message = {"error": "socketServerDecoding"}
+                finally:
+                    # Send message to external handler
+                    loop.create_task(self._eventHandler.handleEvent(message))
+
+    async def _writeLoop(self):
+        """Encodes and sends messages stored in the queue.
+
+        Sends {"error": "socketServerEncoding"} to message handler if there
+        is an error when serializing and encoding the dictionary.
+        """
+        while True:
+            message = await self.sendQueue.get()
+            try:
+                messageEncoded = (json.dumps(message) +
+                                  self._EOM).encode(self._encoding)
+            except:
+                loop = asyncio.get_running_loop()
+                loop.create_task(self._eventHandler.handleEvent(
+                    {"error": "socketServerEncoding"}))
+            else:
+                self._writer.write(messageEncoded)
+                await self._writer.drain()
---
+++ python-scripts/production_logs/productionlogs.py
@@ -0,0 +1,115 @@
+import asyncio
+
+from jsonsocketserver import JsonSocketServer
+from mysqlclient import mysqlQuery
+
+PORT = 10001
+
+
+class EventHandler:
+    async def handleEvent(self, event: dict[str, str]):
+        await self._eventQueue.put(event)
+
+    async def loop(self):
+        event = await self._eventQueue.get()
+        if (isinstance(event, dict)):
+            type = next(iter(event))
+
+            if (type == "hmiEvent"):
+                if (event["type"] == "getControlRecipesList"):
+                    list = await self._getControlRecipesList()
+                    asyncio.create_task(
+                        self._server.sendQueue.put({"list": list}))
+                elif (event["type"] == "getControlRecipeDetails"):
+                    id = event["controlRecipeID"]
+                    details = await self._getControlRecipeDetails(id)
+                    asyncio.create_task(
+                        self._server.sendQueue.put({"details": details}))
+
+    def __init__(self, socketServer: JsonSocketServer):
+        self._eventQueue = asyncio.Queue()
+        self._server = socketServer
+
+    async def _getControlRecipesList(self):
+        """Gets list of control recipes
+
+        Returns:
+            List[dict]: Control recipes:
+                [
+                    {"id": ID_1, "masterRecipe": MASTER_RECIPE_NAME_1, "date": DATE_1},
+                    {"id": ID_2, "masterRecipe": MASTER_RECIPE_NAME_2, "date": DATE_2},
+                    ...
+                ]
+        """
+        controlRecipes = await mysqlQuery(
+            """
+            SELECT recetas_control.id_receta_control, recetas_maestras.codigo_receta_maestra,
+                recetas_control.fecha_creada
+            FROM recetas_control
+            INNER JOIN recetas_maestras
+                ON recetas_control.id_receta_maestra = recetas_maestras.id_receta_maestra
+            """
+        )
+        # Values are a list of tuples [(ID_1,RECIPE_NAME_1,DATE_1),(ID_2,RECIPE_NAME_2,DATE_2),...]
+        controlRecipes = [dict(zip(("id", "masterRecipe", "date"), x))
+                          for x in controlRecipes]
+        return controlRecipes
+
+    async def _getControlRecipeDetails(self, id: int):
+        details = await mysqlQuery(
+            """
+            SELECT recetas_maestras.codigo_receta_maestra, usuarios.nombre,
+                recetas_control.fecha_creada, recetas_control.cantidad_producida
+            FROM recetas_control
+            INNER JOIN recetas_maestras
+                ON recetas_control.id_receta_maestra = recetas_maestras.id_receta_maestra
+            INNER JOIN usuarios
+                ON recetas_control.id_usuario = usuarios.id_usuario
+            WHERE recetas_control.id_receta_control = %s
+            """,
+            (id,)
+        )
+        details = dict(
+            zip(("masterRecipe", "user", "date", "produced"), details[0]))
+
+        paramValues = await mysqlQuery(
+            """
+            SELECT parametros.nombre, valores_parametros.valor
+            FROM valores_parametros
+            INNER JOIN parametros
+                ON parametros.id_parametro = valores_parametros.id_parametro
+            WHERE valores_parametros.id_valor_parametro = %s
+            """,
+            (id,)
+        )
+        paramValuesDict = {}
+        # param values are returned as [("P_1_NAME","P_1_VALUE"),(...),...]
+        for i in paramValues:
+            paramValuesDict[i[0]] = i[1]
+        details["parameterValues"] = paramValuesDict
+
+        alarms = await mysqlQuery(
+            """
+            SELECT id_alarma,descripcion,fecha
+            FROM alarmas
+            WHERE id_receta_control = %s
+            """,
+            (id,)
+        )
+        alarmsDict = {}
+        for i in alarms:
+            alarmsDict[str(i[0])] = {"description":i[1],"date":i[2]}
+        details["alarms"] = alarmsDict
+
+        return details
+
+async def main():
+    await asyncio.sleep(5)
+    
+    server = JsonSocketServer(PORT)
+    eventHandler = EventHandler(server)
+    server._eventHandler = eventHandler
+    await server.start()
+    await eventHandler.loop()
+
+asyncio.run(main())
